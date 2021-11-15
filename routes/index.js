77e327074882d6016dab9bcff898f812172a0839
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-11-15 08:47:18 +0500

    country route added to routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const Router = require("express").Router();
 
+const CountyRouter = require("./CountryRoute");
 const HomeRouter = require("./HomeRoute");
 
 Router.use("/", HomeRouter);
+Router.use("/countries", CountyRouter);
 
 module.exports = Router;

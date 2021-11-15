b6afedcdfe55cea2b90d672de537e4720b6f839e
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-11-15 08:45:44 +0500

    added user route and country route

--- modules/pg/pg.js
+++ modules/pg/pg.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require("sequelize");
 const CountryModel = require("../../models/CountryModel");
+const UsersModel = require("../../models/UsersModel");
 const init = require("./init");
+const relations = require("./relations");
 
 if(!process.env.PG_CONNECTION_STRING){
     throw new Error("PG CONNECTION STRING NOT FOUND")
@@ -18,11 +20,13 @@ module.exports = async function pg(){
         let db = {};
 
         db.countries = await CountryModel(sequelize, Sequelize)
+        db.users = await UsersModel(sequelize, Sequelize)
 
 
+        await relations(db)
         await sequelize.sync({ force: false });
 
-	    await init(db)
+	    // await init(db)
 
 
         return db; 
---
+++ modules/pg/relations.js
@@ -0,0 +1,15 @@
+module.exports = async function (db) {
+	db.countries.hasMany(db.users, {
+		foreignKey: {
+			name: "country_id",
+			allowNull: false,
+		},
+	});
+
+	db.users.belongsTo(db.countries, {
+		foreignKey: {
+			name: "country_id",
+			allowNull: false,
+		},
+	});
+};

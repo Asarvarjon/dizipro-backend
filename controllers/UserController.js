89f423eb9ac9dc13660f052e7bab1e5131102472
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-11-21 17:05:24 +0500

    added email recovery controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,8 @@
 const { generateCrypt, compareCrypt } = require("../modules/bcrypt");
 const UserValidations = require("../validations/UserValidations");
 const { createToken } = require("../modules/jsonwebtoken");
+const sendEmail = require("../modules/email");
+const generator = require("generate-password");
 
 module.exports = class UserController {
 	static async UserCreateAccountPostController(req, res, next) {
@@ -101,6 +103,92 @@ module.exports = class UserController {
 
 	static async UserRecoveryPasswordSubmitPostController(req, res, next) {
 		try {
+			const data = await UserValidations.UserForgotPasswordValidation(
+				req.body,
+				res.error
+			);
+
+			const user = await req.db.users.findOne({
+				where: {
+					user_email: data.user_email,
+				},
+			});
+
+			if (!user) throw new res.error(404, "Invalid email");
+
+			const count = await req.db.attempts.count({
+				where: {
+					user_id: user.dataValues.user_id,
+				},
+			});
+
+			if (count > 5) {
+				throw new res.error(429, "Too many requests");
+			}
+
+			const attempt = await req.db.attempts.create({
+				user_id: user.dataValues.user_id,
+			});
+
+			await sendEmail(
+				user.dataValues.user_email,
+				`<a href="${process.env.SITE_URL}/v1/users/password/${attempt.dataValues.attempt_id}">Click to recover</a>`
+			);
+
+			res.status(201).json({
+				ok: true,
+				message: "Confirmation message sent. Check your email.",
+			});
+		} catch (error) {
+			next(error);
+		}
+	}
+
+	static async UserRecoveryPasswordCheckGetController(req, res, next) {
+		try {
+			const { attempt_id } = req.params;
+
+			if (!attempt_id) throw new res.error(404, "Page not found");
+
+			const attempt = await req.db.attempts.findOne({
+				where: {
+					attempt_id,
+				},
+				include: req.db.users,
+			});
+
+			if (!attempt) throw new res.error(404, "Page not found");
+
+			await req.db.attempts.destroy({
+				where: {
+					user_id: attempt.dataValues.user_id,
+				},
+			});
+
+			const new_password = generator.generate({
+				length: 8,
+			});
+
+			await req.db.users.update(
+				{
+					user_password: generateCrypt(new_password),
+				},
+				{
+					where: {
+						user_id: attempt.dataValues.user_id,
+					},
+				}
+			);
+
+			await sendEmail(
+				attempt.dataValues.user.dataValues.user_email,
+				`Your new password: ${new_password}. Please update it!`
+			);
+
+			res.json({
+				ok: true,
+				message: "New password was sent to mail.",
+			});
 		} catch (error) {
 			next(error);
 		}
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -5,4 +5,15 @@ const UserRouter = require("express").Router();
 UserRouter.post("/account", UserController.UserCreateAccountPostController);
 UserRouter.post("/", UserController.UserLoginAccountPostController);
 
+UserRouter.post(
+	"/password",
+	UserController.UserRecoveryPasswordSubmitPostController
+);
+UserRouter.get(
+	"/password/:attempt_id",
+	UserController.UserRecoveryPasswordCheckGetController
+);
+
+module.exports = UserRouter;
+
 module.exports = UserRouter;
